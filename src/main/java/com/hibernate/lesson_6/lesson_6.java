37ae35be4e88273da778a2290da8ed4030787811
PavelKazukhouski <p.kazhukhouski@gmail.com>
2024-10-17 19:44:50 +0200

    lesson 5

--- src/main/java/com/hibernate/lesson_6/lesson_6.java
+++ src/main/java/com/hibernate/lesson_6/lesson_6.java
@@ -1,33 +1,27 @@
-package com.hibernate.lesson_2.entity;
+package com.hibernate.lesson_6;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Main {
+public class lesson_6 {
     public static void main(String[] args) {
-        Car car = Car.builder()
-                .color("black")
-                .price(150_000)
-                .speed(340.0)
-                .year(2022)
-                .name("BMW")
-                .build();
-
-
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Car.class)
                 .buildSessionFactory();
-        Session session = factory.getCurrentSession();
 
         try {
+            Session session = factory.getCurrentSession();
             session.beginTransaction();
-            session.save(car);
+            Car car = session.get(Car.class, 2);
+            System.out.println(car);
+            car.setSpeed(330);
+            car.setName("BMW");
+            System.out.println(car);
             session.getTransaction().commit();
         } finally {
             factory.close();
         }
-        
     }
 }
--- src/main/java/com/hibernate/lesson_7/Lesson_7.java
+++ src/main/java/com/hibernate/lesson_7/Lesson_7.java
@@ -1,33 +1,28 @@
-package com.hibernate.lesson_2.entity;
+package com.hibernate.lesson_7;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Main {
+import java.util.List;
+
+public class Lesson_7 {
     public static void main(String[] args) {
-        Car car = Car.builder()
-                .color("black")
-                .price(150_000)
-                .speed(340.0)
-                .year(2022)
-                .name("BMW")
-                .build();
-
-
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Car.class)
                 .buildSessionFactory();
-        Session session = factory.getCurrentSession();
-
         try {
+            Session session = factory.getCurrentSession();
             session.beginTransaction();
-            session.save(car);
+            List<Car>  cars = session.createQuery("from Car where year = 2021").getResultList();
+
+            for (Car car : cars){
+                System.out.println(car);
+            }
             session.getTransaction().commit();
-        } finally {
+        }finally {
             factory.close();
         }
-        
     }
 }
